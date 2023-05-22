aeec30d312230b3a194ffde858fb2cb86a4a6f04
kavinmk6 <kavinmk6@gmail.com>
2023-05-21 20:29:27 -0400

    Created the skeleton of the project such as fragments, layout, navigation, data class, RetrofitInstance.

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticlesNewsFragment.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticlesNewsFragment : Fragment(R.layout.fragment_article) {
+
+
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "1352705e23d4454599ec9f6ca645cbe7"
+        const val BASE_URL = "https://newapi.org"
+    }
+}
